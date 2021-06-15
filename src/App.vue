08b69fbdd9324f5b7fb7bed3b1953151db250218
neverwinter-sjh <neverwinter.sjh@gmail.com>
2021-06-15 21:50:43 +0900

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,128 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <section>
+    <h1 class="mb-24">Vue Masonry Wall Images</h1>
+
+    <vue-masonry-wall :items="items" :options="options" @append="append">
+      <template v-slot:default="{item}">
+        <div class="Item">
+          <div class="inner-content">
+            <img :src="item.image"/>
+            <div class="Content">
+              <h5 class="text-ellipsis-1l">{{item.title}}</h5>
+              <p class="text-ellipsis-2l">{{item.content}}</p>
+            </div>
+          </div>
+          <div class="fake-bg"></div>
+        </div>
+      </template>
+    </vue-masonry-wall>
+  </section>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  import VueMasonryWall from "vue-masonry-wall";
+  // function content() {
+  //   const length = Math.random() * 300 + 30
+  //   let result = '';
+  //   const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  //   const charactersLength = characters.length;
+  //   for (let i = 0; i < length; i++) {
+  //     result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  //   }
+  //   return result;
+  // }
+  export default {
+    name: "ExampleMasonry",
+    components: {VueMasonryWall},
+    data() {
+      return {
+        options: {
+          width: 300,
+          padding: {
+            2: 8,
+            default: 12
+          },
+        },
+        masonryItems: [],
+        items: [
+          {
+            title: 'Sed non ante non cras amet',
+            content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas non sagittis leo. Vestibulum sit amet metus nec neque dignissim dapibus.',
+            image: 'https://picsum.photos/id/1015/600/600'
+          },
+          {
+            title: 'Curabitur sit amet nunc',
+            content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec id mollis erat. Aliquam erat volutpat. Nunc erat lacus, rhoncus nec.',
+            image: 'https://picsum.photos/id/1019/600/700'
+          },
+          {
+            title: 'Proin pharetra, ante metus',
+            content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi ac diam ac ex efficitur posuere. Pellentesque cursus pellentesque risus, non.',
+            image: 'https://picsum.photos/id/1039/600/800'
+          },
+          {
+            title: 'Cras pharetra non enim a',
+            content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi malesuada varius nibh, a malesuada nisi feugiat eget. Aenean convallis semper.',
+            image: 'https://picsum.photos/id/1042/600/300'
+          },
+        ]
+      }
+    },
+    methods: {
+      append() {
+        for (let i = 0; i < 20; i++) {
+          const id = Math.floor(Math.random() * 1000)
+          const height = Math.floor(Math.random() * 200) + 600
+          this.items.push({
+            title: `Appended ${i}`,
+            content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas non sagittis leo. Vestibulum sit amet metus nec neque dignissim dapibus.',
+            image: `https://picsum.photos/id/${id}/600/${height}`
+          })
+        }
+      },
+      onWindowLoad() {
+        const items = document.querySelectorAll('.masonry-item');
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+        console.log(items);
+        this.masonryItems = items;
+      }
+    },
+    created() {
+      window.addEventListener("load", this.onWindowLoad);
+    },
+    mounted() {
+      console.log('mounted');
+    }
   }
-}
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style scoped>
+  .Item {
+    overflow: hidden;
+    border-radius: 4px;
+    width: 100%;
+    background: #F5F5F5;
+    position:relative;
+  }
+  .Item .inner-content {
+    visibility: hidden;
+  }
+  .Item .fake-bg {
+    position:absolute;
+    left:0;
+    right:0;
+    bottom:0;
+    background:#fff;
+    height:100%;
+  }
+  .Content {
+    padding: 20px;
+  }
+  img {
+    object-fit: cover;
+    width: 100%;
+    height: 100%;
+    line-height: 0;
+    display: block;
+  }
 </style>
---
+++ src/components/ImageItem.vue
@@ -0,0 +1,25 @@
+<template>
+  <div :default="{item}">
+    <div class="Item">
+      <img :src="item.image" />
+
+      <div class="Content">
+        <h5 class="text-ellipsis-1l">{{item.title}}</h5>
+        <p class="text-ellipsis-2l">{{item.content}}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ImageItem',
+  props: {
+    item: Object
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
